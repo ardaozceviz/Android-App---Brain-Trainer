a3015c5a6f62928ba43b3287a1ff7157211b46b7
Arda Özceviz <aozceviz@Ardas-MacBook-Air.local>
2017-04-04 20:59:24 +0300

    Problems are solved, works somoothly

--- app/src/main/java/com/example/aozceviz/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/aozceviz/braintrainer/MainActivity.java
@@ -14,32 +14,6 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Random;
 
-
-class HashtableExample {
-
-    public static void main(String[] args) {
-
-        Enumeration names;
-        String key;
-
-        // Creating a Hashtable
-        Hashtable<String, String> hashtable = new Hashtable<String, String>();
-
-        // Adding Key and Value pairs to Hashtable
-        hashtable.put("Key1","Chaitanya");
-        hashtable.put("Key2","Ajeet");
-        hashtable.put("Key3","Peter");
-        hashtable.put("Key4","Ricky");
-        hashtable.put("Key5","Mona");
-
-        names = hashtable.keys();
-        while(names.hasMoreElements()) {
-            key = (String) names.nextElement();
-            System.out.println("Key: " +key+ " & Value: " + hashtable.get(key));
-        }
-    }
-}
-
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout gameLayout;
@@ -54,11 +28,9 @@ public class MainActivity extends AppCompatActivity {
     TextView timeTextView;
     TextView gameOverScoreTextView;
 
-    //ArrayList<Integer> answers = new ArrayList<>();
-
-    Enumeration answer;
+    Enumeration answers;
     String answerKey;
-    Hashtable<String, String> answers = new Hashtable<String, String>();
+    Hashtable<String, String> answersHashTable = new Hashtable<String, String>();
 
     int[] printButton = new int[4];
     int printButtonCounter = 0;
@@ -66,11 +38,10 @@ public class MainActivity extends AppCompatActivity {
     int incorrectAnswer = 0;
     int score = 0;
     int numberOfQuestions = 0;
-    String givenAnswer = "";
 
     public void generateQuestion () {
 
-        answers.clear();
+        answersHashTable.clear();
         printButtonCounter = 0;
 
         for (int x = 0; x < 4; x++){
@@ -87,22 +58,21 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < 4; i++) {
             if (i == locationOfCorrectAnswer) {
-                answers.put(String.valueOf(a+b), "correct");
-                givenAnswer = String.valueOf(a+b);
+                answersHashTable.put(String.valueOf(a+b), "correct");
             } else {
                 incorrectAnswer = random.nextInt(41);
                 if (incorrectAnswer != a + b){
-                    answers.put(String.valueOf(incorrectAnswer), "incorrect");
-                    givenAnswer = String.valueOf(incorrectAnswer);
+                    answersHashTable.put(String.valueOf(incorrectAnswer), "incorrect");
                 } else {
                     i--;
                 }
             }
         }
 
-        answer = answers.keys();
-        while(answer.hasMoreElements()) {
-            answerKey = (String) answer.nextElement();
+        answers = answersHashTable.keys();
+
+        while(answers.hasMoreElements()) {
+            answerKey = (String) answers.nextElement();
             printButton[printButtonCounter] = Integer.parseInt(answerKey);
             printButtonCounter++;
             //System.out.println("Key: " +answerKey+ " & Value: " + answers.get(answerKey));
@@ -112,45 +82,6 @@ public class MainActivity extends AppCompatActivity {
         button1.setText(Integer.toString(printButton[1]));
         button2.setText(Integer.toString(printButton[2]));
         button3.setText(Integer.toString(printButton[3]));
-
-        /*
-        for (int i = 0; i < 4; i++){
-            if (i == locationOfCorrectAnswer) {
-                answers.add(a + b);
-            } else {
-                incorrectAnswer = random.nextInt(41);
-                if (incorrectAnswer != a + b) {
-                    answers.add(incorrectAnswer);
-                } else {
-                    i--;
-                }
-            }
-        }
-        */
-        /*
-        for (int i = 0; i < 4; i++) {
-            if (i == locationOfCorrectAnswer) {
-                answers.add(a + b);
-            } else {
-                do {
-                    incorrectAnswer = random.nextInt(41);
-                    if (incorrectAnswer == a + b) {
-                        do {
-                            incorrectAnswer = random.nextInt(41);
-                        } while (incorrectAnswer != a + b);
-                    }
-                } while (answers.contains(incorrectAnswer));
-                answers.add(incorrectAnswer);
-            }
-        }
-        */
-
-        /*
-        button0.setText(Integer.toString(answers.get(0)));
-        button1.setText(Integer.toString(answers.get(1)));
-        button2.setText(Integer.toString(answers.get(2)));
-        button3.setText(Integer.toString(answers.get(3)));
-        */
 
     }
 
@@ -189,10 +120,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void chooseAnswer (View view) {
-        System.out.println(answers.get("correct"));
-
-        //if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
-        if(view.getTag().toString().equals(answers.get(givenAnswer))){
+        Log.i("Log: ", String.valueOf(answersHashTable.get(Integer.toString(printButton[Integer.parseInt((String.valueOf(view.getTag())))]))));
+        Log.i("Button: ", Integer.toString(printButton[Integer.parseInt((String.valueOf(view.getTag())))]));
+        if(String.valueOf(answersHashTable.get(Integer.toString(printButton[Integer.parseInt((String.valueOf(view.getTag())))]))).equals("correct")){
             score++;
             numberOfQuestions++;
             resultTextView.setText("Correct!");
